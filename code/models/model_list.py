0c83393b156e93c766b8bb04f4e279de8f76b7a5
cgao-comp <81541997+cgao-comp@users.noreply.github.com>
2025-04-03 23:53:07 +0800

    Delete code/models directory

--- code/models/model_list.py
+++
@@ -1,8 +0,0 @@
-from models.s2i_tune import S2ITune
-from models.s2i_spos_search import S2ISPOSSearch
-from models.s2i_darts import S2IDartsSearch
-MODEL = {
-    "S2ITune": S2ITune,
-    "S2ISPOSSearch": S2ISPOSSearch,
-    "S2IDartsSearch": S2IDartsSearch
-}
